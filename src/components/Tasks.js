476270c0d6dda6a10acf58bdba7ca69a66e98b52
Artur Dolzan Neto <adolzaneto@MacBook-Air.local>
2019-05-19 11:15:12 -0300

    alter

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -100,8 +100,12 @@ const styles = StyleSheet.create({
 
     loading: {
         position: 'absolute',
-        paddingTop: 20,
-        paddingLeft: 20
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
 
     imageContainer: {
